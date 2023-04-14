a839b56b520a5ca92ed3e08465d64a2a67212ac0
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-04-14 12:31:08 +0200

    wip

--- src/File.php
+++ src/File.php
@@ -26,6 +26,11 @@ class File
         Macro::bootstrap();
     }
 
+    public function __unserialize(array $data): void
+    {
+        $this->flush();
+    }
+
     public function prefix(string $prefix): self
     {
         $this->prefix = Hash::md5($prefix);
